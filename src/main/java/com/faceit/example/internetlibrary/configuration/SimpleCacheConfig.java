f0751db6801c2e8f6678786e9da2ec5dce63605e
AlexanderSV91 <alexander.shalamov@ukr.net>
2021-01-28 16:44:45 +0200

    fix @EnableCaching

--- src/main/java/com/faceit/example/internetlibrary/configuration/SimpleCacheConfig.java
+++ src/main/java/com/faceit/example/internetlibrary/configuration/SimpleCacheConfig.java
@@ -1,10 +1,12 @@
 package com.faceit.example.internetlibrary.configuration;
 
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableCaching
 public class SimpleCacheConfig implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
 
     @Override
